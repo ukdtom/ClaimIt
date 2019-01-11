c99e50944ddef8ec81b72412f86122704817e91e
Tommy Mikkelsen <tm@orstrand.dk>
2019-01-11 15:22:39 +0100

    Update claimpms.sh

--- linux/claimpms.sh
+++ linux/claimpms.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #************************************************************************
-#* Script to claim a Plex Media Server
-#* Will prompt for:
-#*     * plex.tv username
-#*     * plex.tv password
-#*     * IP Address of your unclaimed Plex Media Server
-#*
-#*
-#*
-#*
-#* Made by dane22, a Plex community member
+# Script to claim a Plex Media Server
+# Will prompt for:
+#     * plex.tv username
+#     * plex.tv password
+#     * IP Address of your unclaimed Plex Media Server
+#
+# Made by dane22, a Plex community member
 #************************************************************************
 
-# Start with some functions
+#************************************************************************
+# Functions
+#************************************************************************
+
 function ComparePwd()
 # Compare password entered
 {
@@ -26,9 +26,59 @@ function ComparePwd()
 }
 
 function GetClaimToken()
-# Get claim token from plex.tv
+#*****************************************
+#* Get claim token from plex.tv
+#* Needs the following params:
+#* 1 Param: X-Plex-Token
+#* 2 Param: X-Plex-Client-Identifier
+#*****************************************
 {
-  echo "Get claim token"
+  url="https://plex.tv/api/claim/token?X-Plex-Token=$1&X-Plex-Client-Identifier=ClaimIt-$2"
+  local response=$(curl -i -k -L -s $url)
+  # Grap the token
+  local claimtoken=$(printf %s "$response" | awk -F= '$1=="token"{print $2}' RS=' '| cut -d '"' -f 2)
+  # grap the return code
+  local http_status=$(echo "$response" | grep HTTP |  awk '{print $2}')
+  if [ -z "$http_status" ];
+  then
+     exit 1
+  else
+    if [ $http_status -eq "200" ]
+      then
+        echo "$claimtoken"
+        exit 0
+      else
+       exit 1
+    fi
+  fi   
+}
+
+function GetLoginToken()
+#*****************************************
+#* Get user auth token from plex.tv
+#* Needs the following params:
+#* 1 Param: UserName
+#* 2 Param: Password
+#* 3 Param: X-Plex-Client-Identifier
+#*****************************************
+{
+  url="https://plex.tv/api/v2/users/signin?login=$1&password=$2&X-Plex-Client-Identifier=ClaimIt-$3"
+  local response=$(curl -X POST -i -k -L -s $url)
+  # Grap the token
+  local UserToken=$(printf %s "$response" | awk -F= '$1=="authToken"{print $2}' RS=' '| cut -d '"' -f 2)
+  # grap the return code
+  local http_status=$(echo "$response" | grep HTTP |  awk '{print $2}')
+  if [ -z "$http_status" ];
+  then 
+     exit 1
+  else
+    if [ "$http_status" -lt "202" ]
+      then
+        echo "$UserToken"
+      else
+       exit 1
+    fi
+  fi
 }
 
 function GetClientIdentifier()
@@ -40,21 +90,55 @@ function GetClientIdentifier()
   local http_status=$(echo "$content" | grep HTTP |  awk '{print $2}')
   if [ -z "$http_status" ];
   then 
-     echo "Error"
      exit 1
   else
-
     if [ $http_status -eq "200" ]
       then
-        echo "$machineIdentifier"
+          if [ -z "$machineIdentifier" ];
+          then          
+            exit 1
+          else
+            echo "$machineIdentifier"
+          fi
       else
-       echo "Error"
        exit 1
     fi
   fi  
 }
 
-
+function Claimit()
+#*****************************************
+#* Claim a PMS Server
+#* Needs the following params:
+#* 1 Param: $ippms
+#* 2 Param: $XPlexClaimtoken
+#* 3 Param: $XPlexClientIdentifier
+#* 4 Param: $UserToken
+#*****************************************
+{
+  url="http://$1:32400/myplex/claim?token=$2&X-Plex-Client-Identifier=ClaimIt-$3&X-Plex-Token=$4"
+  content=$(curl -X POST -i -k -L -s $url)
+  local http_status=$(echo "$content" | grep HTTP |  awk '{print $2}')
+  if [ -z "$http_status" ];
+  then
+    echo "Fatal error happened"
+    echo "HTTP returned Status $http_status" 
+    exit 1
+  else
+    if [ $http_status -eq "200" ];
+    then
+      echo "Claiming server ok $http_status"
+      echo ""
+      echo ""
+      echo "Please close your browser, reopen, and browse to http://$1:32400/web"
+      exit 0
+    else
+      echo "Fatal error happened"
+      echo "HTTP returned Status $http_status" 
+      exit 1
+    fi   
+  fi  
+}
 
 #************************************************************************
 #* Main
@@ -64,62 +148,61 @@ clear
 echo "************************************************************************"
 echo "* Script to claim a Plex Media Server"
 echo "* Will prompt for"
-echo "#*     * plex.tv username"
-echo "#*     * plex.tv password"
-echo "#*     * IP Address of your unclaimed Plex Media Server"
-echo "#*"
-echo "#*"
-echo "#*"
-echo "#*"
-echo "#* Made by dane22, a Plex community member"
-echo "#************************************************************************"
+echo "*     * plex.tv username"
+echo "*     * plex.tv password"
+echo "*     * IP Address of your unclaimed Plex Media Server"
+echo "*"
+echo "*"
+echo "* Made by dane22, a Plex community member"
+echo "************************************************************************"
 
 
 
-#read -p 'plex.tv Username: ' uservar
+read -p 'plex.tv Username: ' uservar
 echo ''
-#read -sp 'plex.tv Password: ' passvar
+read -sp 'plex.tv Password: ' passvar
 echo ''
-#read -sp 'plex.tv Password Repeated: ' passvar2
+read -sp 'plex.tv Password Repeated: ' passvar2
 echo ''
-#read -p 'IP Address of PMS server: ' ippms
-
-ippms="192.168.1.12"
-
-
-echo ''
-echo ''
-echo ''
+read -p 'IP Address of PMS server: ' ippms
 
 # Compare pwd entered
 echo "Comparing entered passwords"
 ComparePwd
-echo "passwords identical"
-# Get PMS Client Identifier, and eject if error
-echo "Getting PMS identifier"
-XPlexClientIdentifier=$(GetClientIdentifier)
-if [[ $XPlexClientIdentifier == Error* ]];
+echo "Comparing entered passwords ok"
+
+echo "Getting PMS Server Identifier"
+if ! XPlexClientIdentifier=$(GetClientIdentifier);
 then
-  echo "Error contacting your Plex Media Server"
-  echo "Please verify IP Address, firewall, as well as your Plex Media Server is up and running"
+  echo "******** ERROR ********"
+  echo "We failed to get the Plex Media Server ID"
+  echo "Please check server is up and running, as well as the IP address entered"
   exit 1
-else
-  echo "Retrieved PMS identifier ok"
 fi
+echo "Getting PMS Server Identifier ok"
 
+# Get UserToken from plex.tv
+echo "Getting User Token from plex.tv"
+if ! UserToken=$(GetLoginToken "$uservar" "$passvar" "$XPlexClientIdentifier");
+then
+  echo "******** ERROR ********"
+  echo "We failed to authenticate towards plex.tv"
+  echo "Please check username and password, as well as network access"
+  exit 1
+fi
+echo "Getting User Token from plex.tv ok"
 
-GetClaimToken
+# Get the Claim token
+echo "Getting PMS Claim Token"
+if ! XPlexClaimtoken=$(GetClaimToken "$UserToken" "$XPlexClientIdentifier");
+then
+  echo "******** ERROR ********"
+  echo "We failed to get a claim token from plex.tv"
+  echo "Please check username and password, as well as network access"
+  exit 1
+fi
+echo "Getting PMS Claim Token ok"
 
-
-
-
-claimurl="https://$ippms:32400/myplex/claim?token=<TOKEN_FROM_https://www.plex.tv/claim/>&X-Plex-Product=Plex%20Web&X-Plex-Version=3.81.0&X-Plex-Client-Identifier=$XPlexClientIdentifier&X-Plex-Platform=Chrome&X-Plex-Platform-Version=71.0&X-Plex-Sync-Version=2&X-Plex-Device=Linux&X-Plex-Device-Name=Chrome&X-Plex-Device-Screen-Resolution=1920x913%2C1920x1080&X-Plex-Token=XXXXXXXXXXXXXXXXX&X-Plex-Language=en"
-
-
-# echo $claimurl
-
-
-
-
-
-
+# Claiming server
+echo "Claiming server"
+Claimit "$ippms" "$XPlexClaimtoken" "$XPlexClientIdentifier" "$UserToken"
